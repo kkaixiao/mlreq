944be966b3752c7f457fd51e93f8f8588382f1c1
kkaixiao <show-key@qq.com>
2020-07-19 03:00:40 -0600

    first version

--- lr_2d.py
+++ lr_2d.py
@@ -10,26 +10,26 @@ X = []
 Y = []
 
 
-df = pd.read_csv('data/data_2d.csv', sep=',', header=None, prefix='Col')
+df = pd.read_csv('data/data_2d.csv', sep=',', header=None, prefix='col')
 
-# create a bias
-bias = pd.Series([1]*len(df['Col1']))
+# create bias term, given it's x0*w0, it should be all one
+df['bias'] = 1
 
-X = pd.concat([df['Col0'], df['Col1'], bias], axis=1)
-Y = df['Col2']
+X = df[['col0', 'col1', 'bias']]
+
+Y = df['col2']
 
 # see what data looks like
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(np.array(X)[:,0], np.array(X)[:,1], np.array(Y))
-ax.scatter( df['Col0'], df['Col1'], df['Col2'] )
+
+ax.scatter(df['Col0'], df['Col1'], df['Col2'])
 plt.show()
 
 
 # use np function to solve the question to find out weights
 w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
-print(type(w))
-print(w)
+
 Yhat = np.dot(X, w)
 
 
